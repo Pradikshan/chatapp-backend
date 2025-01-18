f066edac1d186de06e759fe29066e5d6308face7
Pradikshan <pradikshan234@gmail.com>
2025-01-18 17:13:10 +0530

    Made minor change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { Server } from "socket.io";
 import cors from "cors";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const httpServer = createServer(app);
 
